19f4cda63a7e6561497a7153c7e8cb4efdfba04b
NelitaPaqueritta <n.semedo-silva@arpege.fr>
2026-05-20 11:37:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+    hostname     = "terrakube-api.platform.local"
+    organization = "TestOrg"
+
+    workspaces {
+      name = "d-mo-terrakube"
+    }
+  }
+
   required_providers {
     random = {
       source  = "hashicorp/random"
@@ -6,6 +15,7 @@ terraform {
     }
   }
 }
+
 # test here number 3
 resource "random_pet" "animal" {
   length    = 5
